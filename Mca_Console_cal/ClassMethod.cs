ebb9b129f705340717a56930c5edfa426f4aac58
Akshansh <akshanshkul7830@gmail.com>
2024-08-08 12:09:33 +0530

    Add project files.

---
+++ Mca_Console_cal/ClassMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mca_Console_cal
+{
+    internal class ClassMethod
+    {
+        public void TestTry()
+        {
+            Console.WriteLine("test try class method");
+        }
+    }
+}
+
+    
