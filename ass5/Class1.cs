6c44dccd3c48d3aaafc6ba0b24066038f7e4c21a
Akshansh <akshanshkul7830@gmail.com>
2024-11-20 14:27:10 +0530

    Add project files.

---
+++ ass5/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass5
+{
+    internal class Class1
+    {
+        public class Car
+        {
+            public delegate void BookingNotify(String message);
+            public void NotifyBilling(string message)
+            {
+                Console.WriteLine("Billing Department :" + message);
+            }
+        }
+    }
+}
---
+++ ass5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static ass5.Class1.Car;
+using static ass5.Class1;
+
+public class OuterClass
+{
+    public string OuterProperty { get; set; }
+
+    public OuterClass(string outerProperty)
+    {
+        OuterProperty = outerProperty;
+    }
+
+    public class NestedClass
+    {
+        public string NestedProperty { get; set; }
+
+        public NestedClass(string nestedProperty)
+        {
+            NestedProperty = nestedProperty;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Nested Property: {NestedProperty}");
+        }
+    }
+
+    public void DisplayOuterProperty()
+    {
+        Console.WriteLine($"Outer Property: {OuterProperty}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       /* OuterClass outer = new OuterClass("Outer Value");
+        OuterClass.NestedClass nested = new OuterClass.NestedClass("Nested Value");
+        outer.DisplayOuterProperty();
+        nested.Display();*/
+
+
+        Car myCar = new Car();
+        BookingNotify notify = myCar.NotifyBilling;
+        notify("Your booking has been confirmed.");
+        Console.ReadLine();
+    }
+}
